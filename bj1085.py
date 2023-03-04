37ef47f2eb0df0bc15a9f87ce0b67964aeaba071
Seohyeon Lee <apple121626@gmail.com>
2023-03-04 11:26:09 +0900

    added do it! studies

--- bj1085.py
+++ bj1085.py
@@ -2,3 +2,8 @@ xywh = [int(i) for i in input().split()]
 list1 = sorted([xywh[0], xywh[1], xywh[2]-xywh[0], xywh[3]-xywh[1]])
 
 print(list1[0])
+
+# another way to solve 
+
+# xywh = [int(i) for i in input().split()]
+# print(min([xywh[0], xywh[1], xywh[2]-xywh[0], xywh[3]-xywh[1]]))
--- bj10869.py
+++ bj10869.py
@@ -7,3 +7,15 @@ print(a-b)
 print(a*b)
 print(int(a/b))
 print(a%b)
+
+# another way using readline
+
+# import sys
+
+# a, b = map(int, sys.stdin.readline().split())
+
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(int(a/b))
+# print(a%b)
