0275294c6c25d4fda7a99cbd9741f3bfc37d0177
Seohyeon Lee <apple121626@gmail.com>
2023-03-11 21:16:51 +0900

    modified #13 9012 괄호

--- Week02/BaekJoon/bj9012.py
+++ Week02/BaekJoon/bj9012.py
@@ -1,52 +1,77 @@
-# 괄호
+# # 괄호
 
-from collections import deque
+# from collections import deque
+# import sys
+# input = sys.stdin.readline
+
+# class Stack:
+#     class Empty(Exception):
+#         pass
+
+#     """고정 길이 스택 클래스(collections.deque)를 사용"""
+#     def __init__(self, maxlen: int = 256) -> None:
+#         """스택 초기화"""
+#         self.capacity = maxlen
+#         self.__stk = deque([], maxlen)
+    
+#     def is_empty(self) -> bool:
+#         """스택이 비어 있는지 판단"""
+#         return not self.__stk
+    
+#     def push(self, value: int) -> None:
+#         """스택에 value를 푸시"""
+#         self.__stk.append(value)
+
+#     def pop(self):
+#         """스택에서 데이터를 팝"""
+#         try:
+#             return self.__stk.pop()
+#         except:
+#             raise Stack.Empty
+    
+#     def clear(self):
+#         """스택을 비움"""
+#         self.__stk.clear()
+        
+# stk = Stack(50)
+# for _ in range(int(input())):
+#     stk.clear()
+#     for char in input().strip():
+#         if char == '(':
+#             stk.push(1)
+#         else:
+#             try:
+#                 stk.pop()
+#             except:
+#                 print('NO')
+#                 break
+#     else:
+#         if stk.is_empty():
+#             print('YES')
+#         else:
+#             print('NO')
+
+
+
+
+# 괄호
+# 다시 풀기
+
 import sys
 input = sys.stdin.readline
 
-class Stack:
-    class Empty(Exception):
-        pass
+def brackets():
+    for _ in range(int(input())):
+        stk = []
 
-    """고정 길이 스택 클래스(collections.deque)를 사용"""
-    def __init__(self, maxlen: int = 256) -> None:
-        """스택 초기화"""
-        self.capacity = maxlen
-        self.__stk = deque([], maxlen)
-    
-    def is_empty(self) -> bool:
-        """스택이 비어 있는지 판단"""
-        return not self.__stk
-    
-    def push(self, value: int) -> None:
-        """스택에 value를 푸시"""
-        self.__stk.append(value)
-
-    def pop(self):
-        """스택에서 데이터를 팝"""
-        try:
-            return self.__stk.pop()
-        except:
-            raise Stack.Empty
-    
-    def clear(self):
-        """스택을 비움"""
-        self.__stk.clear()
-        
-stk = Stack(50)
-for _ in range(int(input())):
-    stk.clear()
-    for char in input().strip():
-        if char == '(':
-            stk.push(1)
+        for char in input().rstrip():
+            if char == '(':
+                stk.append(char)
+            else:
+                if not stk or stk.pop() != '(':
+                    print('NO')
+                    break
         else:
-            try:
-                stk.pop()
-            except:
-                print('NO')
-                break
-    else:
-        if stk.is_empty():
-            print('YES')
-        else:
-            print('NO')
+            if stk: print('NO')
+            else: print('YES')
+brackets()
