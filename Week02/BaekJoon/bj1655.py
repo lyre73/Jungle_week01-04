2d2825d11d411ca3ab02bbc74fd359d3cd33b044
Seohyeon Lee <apple121626@gmail.com>
2023-03-13 21:35:06 +0900

    solved #1655 가운데를 말해요

---
+++ Week02/BaekJoon/bj1655.py
@@ -0,0 +1,19 @@
+# 가운데를 말해요
+import heapq, sys
+input = sys.stdin.readline
+
+leftheap = [float('inf')] # 최대 힙
+rightheap = [float('inf')] # 최소 힙
+
+for _ in range(int(input())):
+    if len(leftheap) <= len(rightheap):
+        heapq.heappush(leftheap, -int(input()))
+    else:
+        heapq.heappush(rightheap, int(input()))
+    if -leftheap[0] > rightheap[0]:
+        leftmax = -heapq.heappop(leftheap)
+        rightmin = heapq.heappop(rightheap)
+        heapq.heappush(leftheap, -rightmin)
+        heapq.heappush(rightheap, leftmax)
+    # print(leftheap, rightheap)
+    print(-leftheap[0])
