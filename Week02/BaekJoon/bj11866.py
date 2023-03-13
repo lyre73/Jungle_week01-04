be8e40b74e34364794e2e8bb313e5c9df0ef00e6
Seohyeon Lee <apple121626@gmail.com>
2023-03-13 11:48:33 +0900

    solved #22 11866 요세푸스 문제 0

---
+++ Week02/BaekJoon/bj11866.py
@@ -0,0 +1,16 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+def josephus():
+    N, K = map(int, input().split())
+    queue = deque(range(1, N+1))
+
+    print('<', end='')
+    while len(queue) > 1:
+        # print(queue)
+        queue.rotate(-K+1)
+        print(queue.popleft(), end=', ')
+        # print(queue)
+    print(queue.popleft(), end='>\n')
+josephus()
