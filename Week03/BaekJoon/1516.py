06513627d78db3045c73237ba2854f11496b4830
Seohyeon Lee <apple121626@gmail.com>
2023-03-22 21:23:47 +0900

    modified 게임 개발

--- Week03/BaekJoon/1516.py
+++ Week03/BaekJoon/1516.py
@@ -5,7 +5,7 @@ input = sys.stdin.readline
 N = int(input())
 build_time = [0 for _ in range(N + 1)]
 G = {}
-G_reverse = {}
+# G_reverse = {}
 indegree = [0 for _ in range(N + 1)]
 for i in range(1, N + 1):
     temp = list(map(int, input().split()))
@@ -16,12 +16,6 @@ for i in range(1, N + 1):
             G[v].append(i) # 필요 건물 -> 다음 건물
         else:
             G[v] = [i]
-        if i in G_reverse.keys():
-            G_reverse[i].append(v) # 다음 건물 -> 필요 건물
-        else:
-            G_reverse[i] = [v]
-# print(indegree)
-# print(build_time)
 
 total_time = [0 for _ in range(N + 1)]
 Q = collections.deque()
@@ -31,15 +25,11 @@ for i in range(1, N + 1):
         total_time[i] = build_time[i]
 while Q:
     u = Q.popleft()
-    if u in G_reverse.keys():
-        temp = 0
-        for x in G_reverse[u]:
-            temp = max(temp, total_time[x])
-        total_time[u] = temp + build_time[u]
 
     if u in G.keys():
         for v in G[u]:
             indegree[v] -= 1
+            total_time[v] = max(total_time[v], total_time[u] + build_time[v]) # oh
             if indegree[v] == 0:
                 Q.append(v)
 print(*total_time[1:], sep='\n')
