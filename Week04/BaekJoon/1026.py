18911975d84cc76b5d1d381ed15e062981605f5b
Seohyeon Lee <apple121626@gmail.com>
2023-03-25 14:56:06 +0900

    solved 보물

---
+++ Week04/BaekJoon/1026.py
@@ -0,0 +1,14 @@
+# 보물
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+A.sort()
+B.sort(reverse=True)
+
+ans = 0
+for i in range(N):
+    ans += A[i] * B[i]
+print(ans)
